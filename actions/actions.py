2e6a277e6e73c5d5518cebce2c2e29358c4aaccb
John <s187285@hsu.edu.hk>
2022-11-03 01:10:04 +0800

    add actions.py and slots

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,29 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionChooseFunction(Action):
+    def name(self) -> Text:
+        return "action_say_function_chosen"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        choice = tracker.get_slot("choice")
+        if not choice:
+            dispatcher.utter_message(text="唔好意思 唔清楚你嘅選擇 :(")
+        else:
+            dispatcher.utter_message(text=f"你選擇了 '{choice}'")
+            
+            # if choice == "1)依照地區推介":
+                
+            # elif choice=="2)依照難度推介":
+                
+        return []
