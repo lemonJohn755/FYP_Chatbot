b75627071105ce661e6ee6313fb7b602f07b401a
John <s187285@hsu.edu.hk>
2022-11-05 02:11:07 +0800

    add database connection in actions.py

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Telegram chatbot with Rasa
 
 Flow:
 1. User say hi to initiate the chatbot to list out available function
+2. Select the function (show routes by district/difficulty)
+    1. If selected "show routes by district"
+        1. Ask user which district
+        2. List out the relevant results
+
+Prerequisite:
+1. Setup MySQL database to store the sample data of hiking route (download at `sample data` folder)
+    - setup MySQL and phpMyAdmin server with `docker-compose.yml` file (type `docker compose up` command in the terminal)
+    - database name: FYP_Chatbot
+    - table name: sample_data
+    - You can insert the sample data from `sample_data\sample_data.sql`
--- actions/actions.py
+++ actions/actions.py
@@ -31,6 +31,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import mysql.connector
 
 class ActionChooseFunction(Action):
     def name(self) -> Text:
@@ -62,6 +63,52 @@ class ActionChooseDistrict(Action):
         district = tracker.get_slot("district")
         
         if district is not None:
-            dispatcher.utter_message(text=f"你選擇了 '{district}'")
+            dispatcher.utter_message(text=f"你選擇了 '{district}'，以下係相關結果")
+            query = ActionChooseDistrict.district_db_query(district)
+            
+            dispatcher.utter_message(text=query)
+
             
         return []
+
+    def district_db_query(district):
+        mydb = mysql.connector.connect(
+            host="0.0.0.0",
+            user="root",
+            password="root",
+            database="FYP_Chatbot"
+        )
+        
+        mycursor = mydb.cursor()
+        sql = "SELECT Route, Difficulty, Length, Score, Link FROM sample_data WHERE District='{}'".format(district)
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        
+        result_return = ""
+        # for x in result:
+        #     print("行山徑：",x[0])
+        #     print("難度：",x[3])
+        #     result_return = x[0] + '\n' + result_return
+        
+        i=0
+        total_num = len(result)
+        for x in result:
+            i=i+1
+            print("")
+            print("第 {}/{} 個結果".format(i, total_num))
+            print('行山徑:',x[0])
+            print('難度:',x[1])
+            print('長度:',x[2])
+            print('評分:',x[3])
+            print('詳情:',x[4])
+            
+            heading = "\n第 {}/{} 個結果".format(i, total_num)+ '\n'
+            route = '行山徑: '+ x[0] + '\n'
+            difficulty = '難度: ' + str(x[1]) + '\n'
+            length = '長度: ' + str(x[2]) + 'km\n'
+            score = '評分: ' + str(x[3]) + '/5\n'
+            detail = '詳情: ' + x[4] + '\n\n'
+            result_return = result_return + heading + route + difficulty + length + score + detail
+            
+        return result_return
+        
